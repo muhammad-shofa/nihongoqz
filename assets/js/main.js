dfea0db9603c97da9746b432970bc182fdf38c26
Muhammad Shofa <mshofa09@gmail.com>
2024-05-13 13:54:20 +0700

    fix some bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,36 +3,73 @@ function changeBg(counter_quiz) {
   cardQuiz.style.backgroundColor = "#0d6efd";
 }
 
+let btnStart = document.getElementById("btn-start");
+
 // trigger content main kana
-// let mainKanaIsSelect = false;
 function mainKana() {
   let contentMainKana = document.getElementById("content-main-kana");
+  let btnMainKana = document.getElementById("btn-main-kana");
+  // let mainKanaIsSelect;
   if (contentMainKana.style.display != "block") {
     contentMainKana.style.display = "block";
-    // mainKanaIsSelect = true;
+    btnMainKana.classList.remove("btn-red");
+    btnMainKana.classList.add("active-btn-red");
+    // mainKanaIsSelect = 1;
   } else {
     contentMainKana.style.display = "none";
-    // mainKanaIsSelect = false;
+    btnMainKana.classList.remove("active-btn-red");
+    btnMainKana.classList.add("btn-red");
+    // mainKanaIsSelect = 0;
   }
 }
+
+// let mainKanaIsSelect = mainKana(mainKanaIsSelect);
 // console.log(mainKanaIsSelect);
 
+// console.log(mainKanaIsSelect);
+// if (mainKanaIsSelect == 1) {
+//   btnStart.style.display = "block";
+// } else {
+//   btnStart.style.display = "none";
+// }
+
 // trigger content dakuten
-// let dakutenIsSelect = false;
+// let dakutenIsSelect = 0;
 function dakuten() {
   let contentDakuten = document.getElementById("content-dakuten");
+  let btnDakuten = document.getElementById("btn-dakuten");
   if (contentDakuten.style.display != "block") {
     contentDakuten.style.display = "block";
-    // return (dakutenIsSelect = true);
+    btnDakuten.classList.remove("btn-red");
+    btnDakuten.classList.add("active-btn-red");
+    // dakutenIsSelect = 1;
+
+    // if (dakutenIsSelect == 1) {
+    //   btnStart.style.display = "block";
+    // } else {
+    //   btnStart.style.display = "none";
+    // }
   } else {
     contentDakuten.style.display = "none";
-    // return (dakutenIsSelect = false);
+    btnDakuten.classList.remove("active-btn-red");
+    btnDakuten.classList.add("btn-red");
+    // dakutenIsSelect = 0;
   }
 }
 // console.log(dakutenIsSelect);
 
+// trigger content combination
+function combination() {
+  let combinationContent = document.getElementById("content-combination");
+  if (combinationContent.style.display != "block") {
+    combinationContent.style.display = "block";
+  } else {
+    combinationContent.style.display = "none";
+  }
+}
+
 // show btn 'start quiz' if main kana or dakuten selected
-// if (mainKanaIsSelect == true || dakutenIsSelect == true) {
+// if (mainKanaIsSelect == true || dakutenIsSelect == 1) {
 //   let btnStart = document.getElementById("btn-start");
 //   btnStart.style.display = "block";
 // }
--- hiragana.php
+++ hiragana.php
@@ -54,24 +54,41 @@ if (isset($_POST['answer_hiragana'])) {
         <!-- container text quiz start -->
         <div class="container-text p-3 mx-auto m-3 justify-content-center rounded-3 shadow text-center">
             <h2><b class="text-red">Hiragana</b> Quiz</h2>
-            <p>Start Your Hiragana Quiz With Nihongoqz.</p>
+            <p>Start Your Hiragana Quiz With Nihongoqz. <br> Type the Romaji in input box kana</p>
             <h4>Select Kana</h4>
             <!-- <div class="container-all-tab mx-auto"> -->
             <!-- trigger select kana start -->
             <div class="d-flex gap-3 justify-content-center">
-                <button class="btn-red py-2 px-3" onclick="mainKana()">Main Kana</button>
-                <button class="btn-red py-2 px-3" onclick="dakuten()">Dakuten</button>
+                <button class="btn-red py-2 px-3" id="btn-main-kana" onclick="mainKana()">Main Kana</button>
+                <button class="btn-red py-2 px-3" id="btn-dakuten" onclick="dakuten()">Dakuten</button>
+                <button class="btn-red py-2 px-3" onclick="combination()">Combination</button>
             </div>
             <!-- trigger select kana end -->
 
-            <!-- content main kana start -->
             <div class="d-flex gap-3 justify-content-center">
-                <div class="content-main-kana" id="content-main-kana">
-                    <h4>Ini main kana</h4>
+                <!-- content main kana start -->
+                <div class="content-main-kana my-3" id="content-main-kana">
+                    <p class="fs-5 border border-3 border-danger py-2 px-3 text-underline-none">Ka/か</p>
+                    <p class="fs-5 border border-3 border-danger py-2 px-3 text-underline-none">Sa/さ</p>
+                    <p class="fs-5 border border-3 border-danger py-2 px-3 text-underline-none">Ta/た</p>
+                    <p class="fs-5 border border-3 border-danger py-2 px-3 text-underline-none">Na/な</p>
+                    <p class="fs-5 border border-3 border-danger py-2 px-3 text-underline-none">Ha/は</p>
                 </div>
-                <div class="content-dakuten" id="content-dakuten">
-                    <h4>Ini dakuten</h4>
+                <!-- content main kana end -->
+                <!-- content dakuten start -->
+                <div class="content-dakuten my-3" id="content-dakuten">
+                    <p class="fs-5 border border-3 border-danger py-2 px-3 text-underline-none">Ga/が</p>
+                    <p class="fs-5 border border-3 border-danger py-2 px-3 text-underline-none">Za/ざ</p>
+                    <p class="fs-5 border border-3 border-danger py-2 px-3 text-underline-none">Da/だ</p>
+                    <p class="fs-5 border border-3 border-danger py-2 px-3 text-underline-none">Ha/は</p>
+                    <p class="fs-5 border border-3 border-danger py-2 px-3 text-underline-none">Pa/ぱ</p>
                 </div>
+                <!-- content dakuten end -->
+                <!-- content combination start -->
+                <div class="content-combination my-3" id="content-combination">
+                    <p>coming soon</p>
+                </div>
+                <!-- content combination end -->
             </div>
             <div class="btn-start" id="btn-start">
                 <button class="btn-red py-2 px-3 my-2">Start Quiz</button>
@@ -109,9 +126,6 @@ if (isset($_POST['answer_hiragana'])) {
     <!-- js -->
     <!-- custom js -->
     <script src="assets/js/main.js"></script>
-    <!-- <script>
-       
-    </script> -->
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
