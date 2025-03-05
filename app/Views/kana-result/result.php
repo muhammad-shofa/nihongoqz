42ff1a08b7959356938c7aefb8c4b56322f0be99
mshofadev <mshofa09@gmail.com>
2025-03-05 09:15:22 +0700

    finishing UI and content result test  based on localstorage data

--- app/Views/kana-result/result.php
+++ app/Views/kana-result/result.php
@@ -18,44 +18,33 @@
     <?= view('components/navbar') ?>
 
     <div class="container bg-light-subtle">
-        <h1 class="my-5 text-center">Your Result</h1>
         <!-- result test -->
-        <div id="container-card" class="d-flex justify-content-evenly align-items-center m-3 p-2 gap-3 rounded border border-5">
-            <div>
-                <div class="pl-5">
-                    <h3>11/10</h2>
-                    <h3>66,66%</h3>
-                    <h3>Moderate</h3>
-                    <p>You did well, scoring 10 out of 15, achieving a solid 66.66%. Your proficiency is at a moderate level, which means you have a good understanding but still have room for improvement. Keep practicing, and you’ll reach a higher level soon!</p>
+        <div id="container-result-card" class="mt-5 rounded">
+            <h1 class="text-center">Your Hiragana Test Result</h1>
+            <div class="d-flex justify-content-evenly align-items-center gap-3 rounded">
+                <div class="px-5 mx-5">
+                    <h3><svg xmlns="http://www.w3.org/2000/svg" style="width: 25px;" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                            <path fill="#2ec27e" d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
+                        </svg> <b id="score-v"></b></h2>
+                        <h3><svg xmlns="http://www.w3.org/2000/svg" style="width: 25px;" viewBox="0 0 640 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path fill="#1c71d8" d="M128 64c0-17.7 14.3-32 32-32l160 0c17.7 0 32 14.3 32 32l0 352 96 0 0-160c0-17.7 14.3-32 32-32l128 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-96 0 0 160c0 17.7-14.3 32-32 32l-160 0c-17.7 0-32-14.3-32-32l0-352-96 0 0 160c0 17.7-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l96 0 0-160z" />
+                            </svg> <b id="percentage-v"></b></h3>
+                        <h3><svg xmlns="http://www.w3.org/2000/svg" style="width: 25px;" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path fill="#f5c211" d="M288 32c0 17.7 14.3 32 32 32l32 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128c-17.7 0-32 14.3-32 32s14.3 32 32 32l320 0c53 0 96-43 96-96s-43-96-96-96L320 0c-17.7 0-32 14.3-32 32zm64 352c0 17.7 14.3 32 32 32l32 0c53 0 96-43 96-96s-43-96-96-96L32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-32 0c-17.7 0-32 14.3-32 32zM128 512l32 0c53 0 96-43 96-96s-43-96-96-96L32 320c-17.7 0-32 14.3-32 32s14.3 32 32 32l128 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-32 0c-17.7 0-32 14.3-32 32s14.3 32 32 32z" />
+                            </svg> <b id="proficiency-v"></b></h3>
+                        <p class="mt-5" id="summary-v"></p>
+                        <!-- btn restart test -->
+                        <button class="btn-restart btn btn-warning rounded mb-5">Restart Test</button>
+                        <p>Made with <svg xmlns="http://www.w3.org/2000/svg" style="width: 18px;" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path fill="#e01b24" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+                            </svg> by <a href="https://mshofadev.vercel.app/" class="text-decoration-none text-dark" target="blank">mshofadev</a></p>
                 </div>
-                <!-- <table>
-                    <tbody>
-                        <tr>
-                            <td>Score</td>
-                            <td>:</td>
-                            <td>10/15</td>
-                        </tr>
-                        <tr>
-                            <td>Percentage</td>
-                            <td>:</td>
-                            <td>66,66%</td>
-                        </tr>
-                        <tr>
-                            <td>Proficiency</td>
-                            <td>:</td>
-                            <td class="fw-bold text-success">High</td>
-                        </tr>
-                    </tbody>
-                </table> -->
-            </div>
-            <div>
-                <img src="<?= base_url('img/japanese_person_2.jpg') ?>" alt="result img" width="500px">
+                <div>
+                    <img src="<?= base_url('img/japanese_person_2.jpg') ?>" alt="result img" class="rounded" width="500px">
+                </div>
             </div>
         </div>
-        <!-- btn restart test -->
-        <div class="text-center">
-            <button class="btn-restart btn btn-lg btn-warning rounded m-3">Restart</button>
-        </div>
+
     </div>
 
     <!-- Bootstrap -->
--- public/js/result.js
+++ public/js/result.js
@@ -33,9 +33,34 @@ $(document).ready(function () {
             let correct_count_result = localStorage.getItem("correct_count_result");
             let total_questions_result = localStorage.getItem("total_questions_result");
             let percentage_result = localStorage.getItem('percentage_result');
-            console.log(correct_count_result);
-            console.log(total_questions_result);
-            console.log(percentage_result);
+            let proficiency = "";
+            let summary= "";
+
+            switch (true) {
+                case (percentage_result <= 45) :
+                    proficiency = "Low";
+                    summary = `Your score is ${correct_count_result} out of ${total_questions_result}, with a percentage of ${percentage_result}%. Your proficiency level is ${proficiency}, indicating that you might need more practice. Don't be discouraged keep learning and improving, and you'll see progress soon!`;
+                    break;
+                case (percentage_result <= 80) :
+                    proficiency = "Moderate";
+                    summary = `You did well, scoring ${correct_count_result} out of ${total_questions_result}, achieving a solid ${percentage_result}%. Your proficiency is at a ${proficiency} level, which means you have a good understanding but still have room for improvement. Keep practicing, and you’ll reach a higher level soon!`;
+                    break;
+                default:
+                    proficiency = "High";
+                    summary = `Excellent work! You scored ${correct_count_result} out of ${total_questions_result}, reaching an impressive ${percentage_result}%. Your proficiency level is ${proficiency}, showing a strong grasp of the material. Keep up the great work and strive for perfection!`;
+                    break;
+            }
+
+            // debug
+            // console.log(correct_count_result);
+            // console.log(total_questions_result);
+            // console.log(percentage_result);
+
+            $('#score-v').text(correct_count_result + "/" + total_questions_result);
+            $('#percentage-v').text(percentage_result + "%");
+            $('#proficiency-v').text(proficiency);
+            $('#summary-v').text(summary);
+
             // let resultCard = `
 
             // `;
